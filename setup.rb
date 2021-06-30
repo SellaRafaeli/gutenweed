74691a7122e0e6b44962d499ede6a253f985d7fd
Sella Rafaeli <sella.rafaeli@gmail.com>
2021-06-30 12:16:20 +0300

    auto-message

--- setup.rb
+++ setup.rb
@@ -1,5 +1,5 @@
 $prod       = settings.production? #RACK_ENV==production?
-$prod_url   = 'https://gutenweed.com'
+$prod_url   = 'https://good-weed.co'
 $root_url   = $prod ? $prod_url : 'http://localhost:4200'
 
 ONE_YEAR_IN_SECONDS = 31556952
