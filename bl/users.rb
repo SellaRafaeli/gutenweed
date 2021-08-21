17325bebd02698c42e0f69cedbc80bd34cbc881f
Sella Rafaeli <sella.rafaeli@gmail.com>
2021-08-21 12:52:22 +0300

    auto-message

--- bl/users.rb
+++ bl/users.rb
@@ -6,7 +6,7 @@ USER_KEYS = ["email",  "name", "handle", "img_url", "timezone",
 	"contact_me", "title", "subtitle", "desc", "lang", "country",
 	 "location", "my_theme", "media", "payout_info", "tags", 
 	 "media_object_fit", "license_url", "license_filename", 'license_numbers', 
-	 'license_text', 'delivery_area', 'active'] + SOCIAL_NETWORKS
+	 'license_text', 'delivery_area', 'active', 'contact','city'] + SOCIAL_NETWORKS
 
 #DEFAULT_IMG = DEFAULT_PIC = '/img/profile.png'
 DEFAULT_IMG = DEFAULT_PIC = '/img/leaf.svg'
