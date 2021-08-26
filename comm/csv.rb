5f47380b27a21d1de16b20c7de5064b000b215a0
Sella Rafaeli <sella.rafaeli@gmail.com>
2021-08-26 13:14:20 +0300

    add csv users script

--- comm/csv.rb
+++ comm/csv.rb
@@ -9,19 +9,31 @@ def upsert_users_from_csv(path)
 	headers = rows[0].map {|s| s.downcase }
 	
 	rows.each_with_index do |row,idx|
-		email = row[5].to_s.downcase
+		next if idx == 0
+		email = row[2].to_s.downcase
 		next unless email.present?
 		data = {
 			csv: true,
+			csv_version: 'disp2',
 			name: row[1],
-			address: row[2],
-			phone: row[4],
-			email: email
+			email: row[2],
+			phone: row[3],			
+			address: row[4],
+			city: row[5].to_s.capitalize,
+			state: row[6],
+			website: row[7],
+			license_numbers: row[8],
 		}
 		
-		# $users.update_one({email: email}, data, upsert: true)
-		user = $users.get(email: email)
-		puts "no user for email #{email}" unless user 
+		puts "#{idx}: #{email}"
+		$users.delete_one({email: email})
+		begin
+			$users.update_one({email: email}, {'$set': data}, upsert: true)
+		rescue => e 
+			puts e.to_s.red
+		end
+		# user = $users.get(email: email)
+		# puts "no user for email #{email}" unless user 
 		# puts "idx: #{idx}, updating #{email}, user_id: #{$users.get(email: email)[:_id]}"
 	end
 
@@ -29,4 +41,4 @@ def upsert_users_from_csv(path)
 end
 
 # path = '~/Downloads/data_ca_1.csv'
-# >> upsert_users_from_csv('~/Downloads/data_ca_1.csv'); data.size
+# >> upsert_users_from_csv('~/Downloads/disp2.csv');
