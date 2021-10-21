f2de2d3c813c8f637b0177cf882b5b8fad42a3ed
Sella Rafaeli <sella.rafaeli@gmail.com>
2021-10-22 00:13:07 +0300

    auto-message

--- db/redis.rb
+++ db/redis.rb
@@ -2,7 +2,7 @@ TEN_SECONDS = 10
 ONE_HOUR_IN_SECONDS = 3600
 ONE_WEEK_IN_SECONDS = ONE_HOUR_IN_SECONDS * 24 * 7
 
-$redis = ($prod ? Redis.new(url: REDIS_URL) : Redis.new) rescue OpenStruct.new
+$redis = ($prod ? Redis.new(url: ENV['REDIS_URL']) : Redis.new) rescue OpenStruct.new
 
 # all keys and values are strings. 
 # $redis.set(key, val)
