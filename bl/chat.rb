3193879223ee2a6b8a8012c72db7cc475f597574
Sella Rafaeli <sella.rafaeli@gmail.com>
2021-10-25 12:19:05 +0300

    basic images

--- bl/chat.rb
+++ bl/chat.rb
@@ -171,6 +171,7 @@ def gen_default_msg_text
 end
 
 PROFILE_PICS=['https://i.imgur.com/dVQqf0w.jpg', 'https://i.imgur.com/BjSL4y7.jpg', 'https://i.imgur.com/HdNglT0.jpg', 'https://i.imgur.com/ji1DMWG.jpg', 'https://i.imgur.com/QdoiKdQ.jpg', 'https://i.imgur.com/nr3SmN1.jpg', 'https://i.imgur.com/M2o9OAr.jpg', 'https://i.imgur.com/kEWklZj.jpg', 'https://i.imgur.com/MG9VqR7.jpg', 'https://i.imgur.com/zEaIG3F.jpg', 'https://i.imgur.com/Rjq4PDR.jpg', 'https://i.imgur.com/MyE4ahG.jpg', 'https://i.imgur.com/461CzUV.jpg', 'https://i.imgur.com/xPrkGVn.jpg', 'https://i.imgur.com/rdYaRDN.jpg', 'https://i.imgur.com/HYweafn.jpg', 'https://i.imgur.com/pEwcfMH.jpg', 'https://i.imgur.com/Iopcsd2.jpg', 'https://i.imgur.com/OHCdzow.jpg', 'https://i.imgur.com/w49BkF8.jpg', 'https://i.imgur.com/Ptkk76P.jpg', 'https://i.imgur.com/lYWTnTB.jpg', 'https://i.imgur.com/T3Foo3X.jpg' ]
+PRODUCT_PICS=['https://i.imgur.com/4JOG6XV.jpg', 'https://i.imgur.com/anFBkBa.jpg', 'https://i.imgur.com/PXE0GRH.jpg', 'https://i.imgur.com/k3Kirje.jpg,' 'https://i.imgur.com/ppsCrx5.jpg', 'https://i.imgur.com/u3sooX9.jpg', 'https://i.imgur.com/tRn8M65.jpg', 'https://i.imgur.com/Vb6MQTo.jpg', 'https://i.imgur.com/2M8sUv2.jpg', 'https://i.imgur.com/3zAeWU0.jpg', 'https://i.imgur.com/XoFxN3L.jpg', 'https://i.imgur.com/c0H2osH.jpg', 'https://i.imgur.com/JO7wr9E.jpg', 'https://i.imgur.com/oKRNgmG.jpg', 'https://i.imgur.com/xW9t8pL.jpg', 'https://i.imgur.com/WZ2FZbV.jpg', 'https://i.imgur.com/35irQEc.jpg', 'https://i.imgur.com/uYVUnNg.jpg', 'https://i.imgur.com/ARJtBkE.jpg', 'https://i.imgur.com/f26vzR6.jpg', 'https://i.imgur.com/9zG8nZv.jpg', 'https://i.imgur.com/T11jNFO.jpg', 'https://i.imgur.com/QFd7YUX.jpg', 'https://i.imgur.com/LucTxwQ.jpg']
 
 def ensure_default_users(cast_id = nil)
 	users  = []
@@ -204,7 +205,9 @@ def generate_default_chat_msgs(channel_id)
 		safe_name = name.to_s.gsub(/[^0-9a-z]/i, '').downcase
 		id   = "chat_#{safe_name}_#{cast_id}_msg_#{i}_#{nice_id}"
 		msg  = {_id: id, seed: true, cast_id: "#{cast_id}", "user_id"=>"#{user[:_id]}", 
-		"name"=>user[:name], "img_url"=>user[:img_url], "type"=>"seller", "message"=>gen_default_msg_text, "status"=>"chat_msg_ok", "created_at"=>time}
+		"name"=>user[:name], "img_url"=>user[:img_url], "type"=>"seller", "message"=>gen_default_msg_text, 
+		msg_body_img: PRODUCT_PICS.sample,
+		"status"=>"chat_msg_ok", "created_at"=>time}
 		
 		[:address, :website, :contact].each {|f| msg[f] = user[f] }
 		msg  = msg.hwia
